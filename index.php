2ce28bf0d067c055cceecb30514ee6466929ae36
Mohamed Housni <mohamed.housni@um5s.net.ma>
2019-02-12 14:06:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Todos</title>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.4/vue.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue"></script>
 </head>
 <body>
 
